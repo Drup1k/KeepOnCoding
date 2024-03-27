fdfbf355312ac3cf62e3a08681c96fe9677f1bd8
Andrej <andrej.zhdkv@gmail.com>
2024-03-27 16:00:57 +0100

    Add commandlinerunner

--- src/main/java/KeepOnCoding/DynamicApp/DynamicApplication.java
+++ src/main/java/KeepOnCoding/DynamicApp/DynamicApplication.java
@@ -1,7 +1,9 @@
 package KeepOnCoding.DynamicApp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DynamicApplication {
@@ -9,6 +11,13 @@ public class DynamicApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(DynamicApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hi from CommandLineRunner");
+		};
 	}
 
 }
